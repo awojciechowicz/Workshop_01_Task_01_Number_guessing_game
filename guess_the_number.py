62d124f345be1c821ff10c9c9cb477e27a399874
awojciechowicz <adrianwojciechowicz@gmail.com>
2024-11-02 10:38:19 +0100

    Two functions added

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,11 +1,27 @@
 from random import randint
 
+def let_me_guess():
+    while True:
+        guess = input("Guess the number: ")
+        try:
+            guess = float(guess)
+            return guess
+        except ValueError:
+            print("It's not a number")
+
+def check_the_value(number, guess):
+    if guess < number:
+        # print("Too small!")
+        return "Too small!"
+    elif guess > number:
+        # print("Too big!")
+        return "Too big!"
+    elif guess == number:
+        # print("You win!")
+        return "You win!"
+
 number = randint(1, 100)
 
-while True:
-    guess = input("Guess the number: ")
-    try:
-        guess = float(guess)
-        break
-    except ValueError:
-        print("It's not a number")
+# my_guess = let_me_guess()
+
+check_the_value(number, let_me_guess())
